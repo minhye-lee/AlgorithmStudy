59c8ec5533cdf2049cad0e1a45f21d7c9a040a74
minhye Lee <minhyelee@minhyeui-MacBookPro.local>
2019-02-09 17:03:23 +0900

    add bracket.py

---
+++ algorithm_basic/data_structure/bracket.py
@@ -0,0 +1,22 @@
+def check_bracket(_bracket):
+    _cnt_left_bracket = 0
+    _cnt_right_bracket = 0
+    for element in _bracket:
+        if element == '(':
+            _cnt_left_bracket += 1
+        elif element == ')':
+            _cnt_right_bracket += 1            
+        if _cnt_left_bracket < _cnt_right_bracket:
+            return False
+    if _cnt_left_bracket != _cnt_right_bracket:
+        return False 
+    
+    return True
+
+num = int(input())
+for i in range(num):
+    bracket = list(input())
+    if check_bracket(bracket):
+        print('YES')
+    else:
+        print('NO')
